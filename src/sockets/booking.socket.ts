b7adcab177fd1875bcae220aba6a73147209ff2b
chandrajeetyadav12 <chandrajeet.yadav@crowdapps.in>
2026-08-05 15:26:48 +0530

    feat: enable booking event registration in socket implementation

--- src/sockets/booking.socket.ts
+++ src/sockets/booking.socket.ts
@@ -1,26 +1,26 @@
-// import { Server } from "socket.io";
+import { Server } from "socket.io";
 
-// export const registerBookingEvents = (
-//   io: Server
-// ) => {
-//   io.on("connection", (socket) => {
-//     console.log(
-//       `Booking Socket Connected: ${socket.id}`
-//     );
+export const registerBookingEvents = (
+  io: Server
+) => {
+  io.on("connection", (socket) => {
+    console.log(
+      `Booking Socket Connected: ${socket.id}`
+    );
 
-//     socket.on(
-//       "join-event",
-//       (eventId: string) => {
-//         socket.join(eventId);
+    socket.on(
+      "join-event",
+      (eventId: string) => {
+        socket.join(eventId);
 
-//         console.log(
-//           `${socket.id} joined ${eventId}`
-//         );
-//       }
-//     );
+        console.log(
+          `${socket.id} joined ${eventId}`
+        );
+      }
+    );
 
-//     socket.on("leave-event", (eventId) => {
-//       socket.leave(eventId);
-//     });
-//   });
-// };
+    socket.on("leave-event", (eventId) => {
+      socket.leave(eventId);
+    });
+  });
+};
